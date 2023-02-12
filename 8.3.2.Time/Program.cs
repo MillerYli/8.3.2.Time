f70630a894fc855c9e3d9e957f5cc5272d89384d
Yulia Miller <miller82.98@mail.ru>
2023-02-11 22:01:27 -0800

    Добавьте файлы проекта.

---
+++ 8.3.2.Time/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Task2
+{
+    public static void Main()
+    {
+        var fileInfo = new FileInfo("/Users/mille/SkillFactory/module_7/7.1.10_task/7.1.10_task/Program.cs");
+
+        using (StreamWriter sw = fileInfo.AppendText())
+        {
+            sw.WriteLine();
+            sw.WriteLine($"//Время запуска: {DateTime.Now}");
+        }
+
+        using (StreamReader sr = fileInfo.OpenText())
+        {
+            string str = "";
+            while ((str = sr.ReadLine()) != null)
+                Console.WriteLine(str);
+        }
+    }
+}
